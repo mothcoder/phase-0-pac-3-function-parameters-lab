5572ae881e56109bc4e17bda8772d07c0292566c
Athena Maynard <xfifix17@gmail.com>
2024-04-25 15:28:36 -0400

    adds myfile.md

---
+++ index.js
@@ -0,0 +1,23 @@
+// value1 = "Aki";
+// value2 = "Ember.js";
+function introduction(name) {
+    return `Hi, my name is ${name}.`; 
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+} 
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") 
+     {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+
+}
+
+
+
+// console.log(indroductionWithLanguage(value1, value2));
+
